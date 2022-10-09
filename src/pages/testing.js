bf20411d38da4c53b742235a640338a7f630606d
VHromyk <65778221+VHromyk@users.noreply.github.com>
2022-10-10 00:49:22 +0300

    crated context

---
+++ src/pages/testing.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout/Layout"
+import Gallery from "../examples/gallery"
+
+const Testing = () => {
+  return (
+    <Layout>
+      <main className="page">
+        <Gallery />
+      </main>
+    </Layout>
+  )
+}
+
+export default Testing
